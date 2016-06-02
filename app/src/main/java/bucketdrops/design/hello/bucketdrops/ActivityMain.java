a7c7cca53b3f3476f6aaacc76bbd0da3a0a08bea
Adebiyi Oluwatobi <tobiemmanuel44@yahoo.com>
2016-06-02 00:04:23 -0400

    No Changes made yet...

--- app/src/main/java/bucketdrops/design/hello/bucketdrops/ActivityMain.java
+++ app/src/main/java/bucketdrops/design/hello/bucketdrops/ActivityMain.java
@@ -3,14 +3,42 @@ package bucketdrops.design.hello.bucketdrops;
 import android.graphics.Matrix;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
+
+    Toolbar mToolbar;
+    Button mBtnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        setSupportActionBar(mToolbar);  /*  Set ActionBar   */
+        initBackgroundImage(); /* Background Method */
 
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivityMain.this, "Button was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*  Set the background for the Home View    */
+    private void initBackgroundImage() {
+        ImageView background = (ImageView) findViewById(R.id.iv_background);
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(background);
     }
 }
